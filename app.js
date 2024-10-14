1792649572d4b0e054350fb1fc2164333ee6351c
Caio Silva <136451281+CaioEdimarSantosSilva@users.noreply.github.com>
2024-10-14 08:51:01 -0300

    .

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
+const Prova = require('./models/prova');
 
 mongoose.connect('mongodb://localhost:27017/provaDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
